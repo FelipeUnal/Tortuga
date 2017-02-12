353aadcaab374f9218fade3f777897a801d5ac72
FelipeUnal <fsescalantet@unal.edu.co>
2017-02-11 21:52:48 -0500

    Agregado

---
+++ src/ejerciciopoo/POO.java
@@ -0,0 +1,121 @@
+package ejerciciopoo;
+
+import ch.aplu.turtle.Turtle;
+
+/**
+ *
+ * @author Equipo
+ */
+public class POO {
+   private  Turtle joe;
+   
+   public POO(){
+     this.joe =  new Turtle(); 
+   }
+   
+   public void dibujar(){
+      joe.penUp();
+      joe.back(50);
+      joe.left(90);
+      joe.forward(75);
+      joe.right(90);
+      joe.penDown();
+      
+      joe.forward(100);
+      joe.right(90);
+      joe.forward(150);
+      joe.right(90);
+      joe.forward(100);
+      joe.right(90);
+      joe.forward(150);
+      
+      joe.back(40);
+      joe.right(90);
+      joe.forward(40);
+      joe.right(90);
+      joe.forward(70);
+      joe.right(90);
+      joe.forward(40);
+      
+      joe.penUp();
+      joe.back(100);
+      joe.left(90);
+      joe.forward(10);
+      joe.left(90);
+      joe.penDown();
+      
+      joe.forward(40);
+      joe.left(90);
+      joe.forward(30);
+      joe.right(60);
+      joe.forward(30);
+      joe.left(120);
+      joe.forward(30);
+      joe.right(60);
+      joe.back(30);
+      joe.forward(60);
+      joe.left(90);
+      joe.forward(40);
+      
+      joe.penUp();
+      joe.forward(40);
+      joe.left(90);
+      joe.forward(10);
+      joe.penDown();
+      joe.left(120);
+      joe.forward(30);
+      joe.left(120);
+      joe.forward(30);
+      joe.left(120);
+      joe.forward(30);
+      
+      joe.penUp();
+      joe.forward(50);
+      joe.penDown();
+      joe.forward(30);
+      joe.left(120);
+      joe.forward(30);
+      joe.left(120);
+      joe.forward(30);
+      
+      joe.penUp();
+      joe.left(30);
+      joe.forward(55);
+      joe.left(90);
+      joe.forward(15);
+      joe.left(90);
+      joe.penDown();
+      joe.forward(30);
+      joe.left(90);
+      joe.forward(25);
+      joe.left(90);
+      joe.forward(30);
+      joe.left(90);
+      joe.forward(25);
+      joe.penUp();
+      
+      joe.back(60);
+      joe.penDown();
+      joe.forward(25);
+      joe.left(90);
+      joe.forward(15);
+      joe.left(90);
+      joe.forward(25);
+      joe.right(90);
+      joe.forward(15);
+      joe.right(90);
+      joe.forward(25);
+      joe.penUp();
+      joe.forward(100);
+      joe.left(90);
+   } 
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       POO e = new POO();
+       e.dibujar();
+    }
+    
+}
